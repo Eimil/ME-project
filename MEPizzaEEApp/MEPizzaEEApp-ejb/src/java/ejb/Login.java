2ae75738a7fbbd57534de05393351a06cc086579
Eimil <eimil_94@hotmail.com>
2015-12-24 11:55:45 +0100

    Initial commit
    Generation of deployment description done manually as transfer goes.

---
+++ MEPizzaEEApp/MEPizzaEEApp-ejb/src/java/ejb/Login.java
@@ -0,0 +1,14 @@
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Emil Ejder
+ */
+@Stateless
+public class Login implements LoginRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
